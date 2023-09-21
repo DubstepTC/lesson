5828937cb1b0b5fbc2c594dca13ee3e16d26d28b
DubstepTC <dubsteptc@mail.ru>
2023-09-22 00:27:55 +0300

    jk

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:lesson/widget/widget.dart";
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Овальные Прямоугольники с Цифрами'),
+          title: Text('Шкаф'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0), 
@@ -21,32 +22,32 @@ class MyApp extends StatelessWidget {
                  Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    OvalRectangle2(number2: '1', width2: 0.455, height2: 0.3,), 
-                    OvalRectangle2(number2: '2', width2: 0.455, height2: 0.3,), 
+                    OvalRectangle2(number2: '1', width2: 0.455, height2: 0.3, colortxt2: Color.fromARGB(255, 166, 48, 224),), 
+                    OvalRectangle2(number2: '2', width2: 0.455, height2: 0.3, colortxt2: Color.fromARGB(255, 0, 0, 0),), 
                   ],
                 ),
                 SizedBox(height: 8),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    OvalRectangle(number: '3', width: 0.455, height: 0.1,), 
-                    OvalRectangle(number: '4', width: 0.455, height: 0.1,), 
+                    OvalRectangle(number: '3', width: 0.455, height: 0.1, colortxt: Color.fromARGB(255, 219, 47, 211),), 
+                    OvalRectangle(number: '4', width: 0.455, height: 0.1, colortxt: Color.fromARGB(255, 214, 153, 40),), 
                   ],
                 ),
                 SizedBox(height: 8), 
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    OvalRectangle(number: '5', width: 0.455, height: 0.1,), 
-                    OvalRectangle(number: '6', width: 0.455, height: 0.1,), 
+                    OvalRectangle(number: '5', width: 0.455, height: 0.1, colortxt: Color.fromARGB(255, 43, 164, 219),), 
+                    OvalRectangle(number: '6', width: 0.455, height: 0.1, colortxt: Color.fromARGB(255, 236, 64, 64),), 
                   ],
                 ),
                 SizedBox(height: 8), 
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    OvalRectangle2(number2: '7', width2: 0.455, height2: 0.3,), 
-                    OvalRectangle2(number2: '8', width2: 0.455, height2: 0.3,), 
+                    OvalRectangle2(number2: '7', width2: 0.455, height2: 0.3, colortxt2: Color.fromARGB(255, 209, 43, 43),), 
+                    OvalRectangle2(number2: '8', width2: 0.455, height2: 0.3, colortxt2: Color.fromARGB(255, 255, 255, 255),), 
                   ],
                 ),
               ],
@@ -58,75 +59,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class OvalRectangle extends StatelessWidget {
-  final String number;
-  final double width;
-  final double height; 
-
-  OvalRectangle({required this.number, required this.width, required this.height});
-
-  @override
-  Widget build(BuildContext context) {
-    // Вычисляем ширину прямоугольника
-    double screenWidth = MediaQuery.of(context).size.width;
-    double rectangleWidth = screenWidth * width; 
-    // Вычисляем высоту прямоугольника
-    double screenHeight = MediaQuery.of(context).size.height;
-    double rectangleHeight = screenHeight * height;
-
-
-    return Container(
-      width: rectangleWidth,
-      height: rectangleHeight,
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 123, 187, 150), 
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Center(
-        child: Text(
-          number,
-          style: TextStyle(
-            fontSize: 24, 
-            color: Colors.white, 
-          ),
-        ),
-      ),
-    );
-  }
-}
-class OvalRectangle2 extends StatelessWidget {
-  final String number2;
-  final double width2;
-  final double height2; 
-
-  OvalRectangle2({required this.number2, required this.width2, required this.height2});
-
-  @override
-  Widget build(BuildContext context) {
-    // Вычисляем ширину прямоугольника
-    double screenWidth2 = MediaQuery.of(context).size.width;
-    double rectangleWidth2 = screenWidth2 * width2; 
-    // Вычисляем высоту прямоугольника
-    double screenHeight2 = MediaQuery.of(context).size.height;
-    double rectangleHeight2 = screenHeight2 * height2;
-
-
-    return Container(
-      width: rectangleWidth2,
-      height: rectangleHeight2,
-      decoration: BoxDecoration(
-        color: Color.fromARGB(255, 71, 233, 125), 
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: Center(
-        child: Text(
-          number2,
-          style: TextStyle(
-            fontSize: 24, 
-            color: Colors.white, 
-          ),
-        ),
-      ),
-    );
-  }
-}
